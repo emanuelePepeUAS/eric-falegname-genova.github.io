670dd38eb11fcb5b465e7dd10f980720a596a353
emanuelePepeUAS <emanuele.pepe@fb2.fra-uas.de>
2026-02-06 11:40:17 +0100

    Set the right url again

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Struttura Rialzata per Letto Futon",
         "excerpt":"Struttura rialzata in legno massello progettata secondo la tradizione giapponese per ospitare materassi futon. Caratteristiche Legno: Rovere o faggio massello Altezza: 30-40 cm da terra per corretta aerazione Design: Minimalista secondo estetica giapponese Dimensioni standard: 140x200 cm (personalizzabile) Garantisce la corretta ventilazione del futon e crea l’atmosfera zen tipica degli...","categories": [],
         "tags": [],
-        "url": "/letti-giapponesi-futon/letto-futon-tatami/",
-        "teaser": "/assets/images/portfolio/letto-giapponese-futon.jpg"
+        "url": "/eric-falegname-genova.github.io/letti-giapponesi-futon/letto-futon-tatami/",
+        "teaser": "/eric-falegname-genova.github.io/assets/images/portfolio/letto-giapponese-futon.jpg"
       },{
         "title": "Sedia di Design",
         "excerpt":"Tavolo con sedie di design realizzate in legno di puffo per test visivo   Dettagli Tecnici     Legno: Puffo   Dimensioni: 200000x100x75 cm   Finitura: Olio innaturale   Tempo realizzazione: 4 anni   Progetto realizzato su misura per cliente di Genova, con possibilità di personalizzare dimensioni e finiture.                                                    Dettagli delle sedie con tavolo       ","categories": [],
         "tags": [],
-        "url": "/lavori-tradizionali/sedia-design/",
-        "teaser": "/assets/images/portfolio/sedia-design.jpg"
+        "url": "/eric-falegname-genova.github.io/lavori-tradizionali/sedia-design/",
+        "teaser": "/eric-falegname-genova.github.io/assets/images/portfolio/sedia-design.jpg"
       },]
